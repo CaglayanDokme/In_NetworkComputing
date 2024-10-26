16c675da589ee0d86ac0e9e82ec937215f05bf7c
CaglayanDokme <caglayandokme@gmail.com>
2024-10-26 13:47:14 +0300

    Simplified testing script.

--- test.sh
+++ test.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Number of times to run the program
-NUM_RUNS=10
-
 # The program to run
 PROGRAM="./build/In_NetworkComputing"
 
-for ((ports=4; ports <= 32; ports+=2)); do
-    for ((i=1; i<=NUM_RUNS; i++)); do
-        ${PROGRAM} --log-filter=3 --ports=${ports} --network-computing=true;
-    done
+for ((ports=4; ports <= 48; ports+=2)); do
+    ${PROGRAM} --log-filter=3 --ports=${ports} --network-computing=true || exit 1;
 done
